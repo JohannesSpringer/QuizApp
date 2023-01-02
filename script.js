5501cd5d45034229f8f6a3ad104fb924ce8bc1f6
JohannesSpringer <johannes.springer.01@gmx.de>
2023-01-02 18:24:56 +0100

    changed navbar styles and added JSON Array

---
+++ script.js
@@ -0,0 +1,46 @@
+let questions = [
+    {
+        "question": 'Was heißt HTML?',
+        "answer_1": '1',
+        "answer_2": '2',
+        "answer_3": '3',
+        "answer_4": '4',
+        "right_answer": '2'
+    },
+    {
+        "question": 'Was heißt HTML?',
+        "answer_1": '',
+        "answer_2": '',
+        "answer_3": '',
+        "answer_4": '',
+        "right_answer": ''
+    },
+    {
+        "question": 'Was heißt HTML?',
+        "answer_1": '',
+        "answer_2": '',
+        "answer_3": '',
+        "answer_4": '',
+        "right_answer": ''
+    },
+    {
+        "question": 'Was heißt HTML?',
+        "answer_1": '',
+        "answer_2": '',
+        "answer_3": '',
+        "answer_4": '',
+        "right_answer": ''
+    },
+    {
+        "question": 'Was heißt HTML?',
+        "answer_1": '',
+        "answer_2": '',
+        "answer_3": '',
+        "answer_4": '',
+        "right_answer": ''
+    },
+];
+
+function init() {
+    
+}
