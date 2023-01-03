e72e3058ae2e95e71da01374ac2eb7dbf18dbafe
JohannesSpringer <johannes.springer.01@gmx.de>
2023-01-03 19:20:43 +0100

    changed style of choices

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let questions = [
     {
+        "question": 'Wer hat HTML erfunden?',
+        "answer_1": 'Robbie Williams',
+        "answer_2": 'Tim Berners - Lee',
+        "answer_3": 'Lady Gaga',
+        "answer_4": 'Justin Bieber',
+        "right_answer": '2'
+    },
+    {
         "question": 'Was heißt HTML?',
         "answer_1": '1',
         "answer_2": '2',
@@ -9,35 +17,27 @@ let questions = [
     },
     {
         "question": 'Was heißt HTML?',
-        "answer_1": '',
-        "answer_2": '',
-        "answer_3": '',
-        "answer_4": '',
-        "right_answer": ''
+        "answer_1": '1',
+        "answer_2": '2',
+        "answer_3": '3',
+        "answer_4": '4',
+        "right_answer": '2'
     },
     {
         "question": 'Was heißt HTML?',
-        "answer_1": '',
-        "answer_2": '',
-        "answer_3": '',
-        "answer_4": '',
-        "right_answer": ''
+        "answer_1": '1',
+        "answer_2": '2',
+        "answer_3": '3',
+        "answer_4": '4',
+        "right_answer": '2'
     },
     {
         "question": 'Was heißt HTML?',
-        "answer_1": '',
-        "answer_2": '',
-        "answer_3": '',
-        "answer_4": '',
-        "right_answer": ''
-    },
-    {
-        "question": 'Was heißt HTML?',
-        "answer_1": '',
-        "answer_2": '',
-        "answer_3": '',
-        "answer_4": '',
-        "right_answer": ''
+        "answer_1": '1',
+        "answer_2": '2',
+        "answer_3": '3',
+        "answer_4": '4',
+        "right_answer": '2'
     },
 ];
 
