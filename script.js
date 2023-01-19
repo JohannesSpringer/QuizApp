fa84f19488eff1abb4579840feb1c0b6d71a9425
JohannesSpringer <johannes.springer.01@gmx.de>
2023-01-19 16:21:11 +0100

    added logic to startScreen, added more questions

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let questions = [
+let questions = [];
+
+// questions HTML
+let questionsQuiz1 = [
     {
         "question": 'Wer hat HTML erfunden?',
         "answer_1": 'Robbie Williams',
@@ -46,22 +49,187 @@ let questions = [
     },
 ];
 
+let questionsQuiz4 = [
+    {
+        "question": 'Welches Amt hat Olaf Scholz?',
+        "answer_1": 'Bundesminister für Wirtschaft und Klimaschutz',
+        "answer_2": 'Bundeskanzler',
+        "answer_3": 'Bundesminister der Finanzen',
+        "answer_4": 'Bundesminister der Justiz',
+        "right_answer": '2',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Robert Habeck?',
+        "answer_1": 'Bundesminister für Wirtschaft und Klimaschutz',
+        "answer_2": 'Bundeskanzler',
+        "answer_3": 'Bundesminister der Finanzen',
+        "answer_4": 'Bundesminister der Justiz',
+        "right_answer": '1',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Christian Lindner?',
+        "answer_1": 'Bundesminister für Wirtschaft und Klimaschutz',
+        "answer_2": 'Bundeskanzler',
+        "answer_3": 'Bundesminister der Finanzen',
+        "answer_4": 'Bundesminister der Justiz',
+        "right_answer": '3',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Nancy Faeser?',
+        "answer_1": 'Bundesministerin für Wirtschaft und Klimaschutz',
+        "answer_2": 'Bundesministerin des Auswärtigen Amtes',
+        "answer_3": 'Bundesministerin des Inneren und der Heimat',
+        "answer_4": 'Bundesminister für Familie, Senioren, Frauen und Jugend',
+        "right_answer": '3',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Annalena Baerbock?',
+        "answer_1": 'Bundesministerin für Wirtschaft und Klimaschutz',
+        "answer_2": 'Bundesministerin des Auswärtigen Amtes',
+        "answer_3": 'Bundesministerin des Inneren und der Heimat',
+        "answer_4": 'Bundesminister für Familie, Senioren, Frauen und Jugend',
+        "right_answer": '2',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Marco Buschmann?',
+        "answer_1": 'Bundesminister für Wirtschaft und Klimaschutz',
+        "answer_2": 'Bundesminister der Verteidigung',
+        "answer_3": 'Bundesminister der Finanzen',
+        "answer_4": 'Bundesminister der Justiz',
+        "right_answer": '4',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Hubertus Heil?',
+        "answer_1": 'Bundesminister für Wirtschaft und Klimaschutz',
+        "answer_2": 'Bundesminister der Verteidigung',
+        "answer_3": 'Bundesminister für Arbeit und Soziales',
+        "answer_4": 'Bundesminister für Gesundheit',
+        "right_answer": '3',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Boris Pistorius?',
+        "answer_1": 'Bundesminister für Ernährung und Landwirtschaft',
+        "answer_2": 'Bundesminister der Verteidigung',
+        "answer_3": 'Bundesminister für Arbeit und Soziales',
+        "answer_4": 'Bundesminister für Gesundheit',
+        "right_answer": '2',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Cem Özdemir?',
+        "answer_1": 'Bundesminister für Ernährung und Landwirtschaft',
+        "answer_2": 'Bundesminister der Verteidigung',
+        "answer_3": 'Bundesminister für Arbeit und Soziales',
+        "answer_4": 'Bundesminister für Gesundheit',
+        "right_answer": '1',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Boris Pistorius?',
+        "answer_1": 'Bundesminister für Ernährung und Landwirtschaft',
+        "answer_2": 'Bundesminister der Verteidigung',
+        "answer_3": 'Bundesminister für Arbeit und Soziales',
+        "answer_4": 'Bundesminister für Gesundheit',
+        "right_answer": '2',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Lisa Paus?',
+        "answer_1": 'Bundesministerin für Wirtschaft und Klimaschutz',
+        "answer_2": 'Bundesministerin des Auswärtigen Amtes',
+        "answer_3": 'Bundesministerin des Inneren und der Heimat',
+        "answer_4": 'Bundesminister für Familie, Senioren, Frauen und Jugend',
+        "right_answer": '4',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Karl Lauterbach?',
+        "answer_1": 'Bundeskanzler',
+        "answer_2": 'Bundesminister der Verteidigung',
+        "answer_3": 'Bundesminister für Arbeit und Soziales',
+        "answer_4": 'Bundesminister für Gesundheit',
+        "right_answer": '4',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Volker Wissing?',
+        "answer_1": 'Bundesminister für besondere Aufgaben / Chef des Bundeskanzleramtes',
+        "answer_2": 'Bundesminister für Umwelt, Naturschutz, nukleare Sicherheit und Verbraucherschutz',
+        "answer_3": 'Bundesminister für Bildung und Forschung',
+        "answer_4": 'Bundesminister für Digitales und Verkehr',
+        "right_answer": '4',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Steffi Lemke?',
+        "answer_1": 'Bundesministerin für besondere Aufgaben / Chefin des Bundeskanzleramtes',
+        "answer_2": 'Bundesministerin für Umwelt, Naturschutz, nukleare Sicherheit und Verbraucherschutz',
+        "answer_3": 'Bundesministerin für Bildung und Forschung',
+        "answer_4": 'Bundesministerin für Digitales und Verkehr',
+        "right_answer": '2',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Bettina Stark-Watzinger?',
+        "answer_1": 'Bundesministerin für besondere Aufgaben / Chefin des Bundeskanzleramtes',
+        "answer_2": 'Bundesministerin für Umwelt, Naturschutz, nukleare Sicherheit und Verbraucherschutz',
+        "answer_3": 'Bundesministerin für Bildung und Forschung',
+        "answer_4": 'Bundesministerin für Digitales und Verkehr',
+        "right_answer": '3',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Svenja Schulze?',
+        "answer_1": 'Bundesministerin für wirtschaftliche Zusammenarbeit und Entwicklung',
+        "answer_2": 'Bundesministerin für Umwelt, Naturschutz, nukleare Sicherheit und Verbraucherschutz',
+        "answer_3": 'Bundesministerin für Wohnen, Stadtentwicklung und Bauwesen',
+        "answer_4": 'Bundesministerin für Digitales und Verkehr',
+        "right_answer": '1',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Klara Geywitz?',
+        "answer_1": 'Bundesministerin für wirtschaftliche Zusammenarbeit und Entwicklung',
+        "answer_2": 'Bundesministerin für Umwelt, Naturschutz, nukleare Sicherheit und Verbraucherschutz',
+        "answer_3": 'Bundesministerin für Wohnen, Stadtentwicklung und Bauwesen',
+        "answer_4": 'Bundesministerin für Digitales und Verkehr',
+        "right_answer": '3',
+        "selected_answer": '',
+    },
+    {
+        "question": 'Welches Amt hat Wolfgang Schmidt?',
+        "answer_1": 'Bundesminister für besondere Aufgaben / Chef des Bundeskanzleramtes',
+        "answer_2": 'Bundesminister für Umwelt, Naturschutz, nukleare Sicherheit und Verbraucherschutz',
+        "answer_3": 'Bundesminister für Bildung und Forschung',
+        "answer_4": 'Bundesminister für Digitales und Verkehr',
+        "right_answer": '4',
+        "selected_answer": '',
+    },
+];
+
 let currentQuestion = 0;
 let AUDIO_SUCCESS = new Audio('audio/success.mp3');
 let AUDIO_FAIL = new Audio('audio/fail.mp3');
 
 function init() {
+    loadQuestions('quiz_1');
     showQuestion();
 }
 
 function showQuestion() {
     if (currentQuestion >= questions.length) {
         calculateScore();
-        calculateProgressBar();
         document.getElementById('endScreen').style = '';
         document.getElementById('questionBody').style = 'display: none;';
     } else {
-        calculateProgressBar();
+        
         let question = questions[currentQuestion];
         document.getElementById('question-text').innerHTML = question['question'];
         document.getElementById('answer_1').innerHTML = question['answer_1'];
@@ -90,6 +258,7 @@ function answer(selection) {
         addClassToAnswerButton(idOfRightAnswerCard, true);
         AUDIO_FAIL.play();
     }
+    calculateProgressBar();
     document.getElementById('next-question').disabled = false;
 
 }
@@ -142,7 +311,7 @@ function calculateScore() {
 }
 
 function calculateProgressBar() {
-    let percent = Math.round(currentQuestion / questions.length * 100);
+    let percent = Math.round((currentQuestion + 1) / questions.length * 100);
     document.getElementById('progressBar').style.width = `${percent}%`;
 }
 
@@ -166,4 +335,31 @@ function chooseQuiz(quizId) {
         document.getElementById(`quiz_${i+1}`).classList.remove('active');    
     }
     document.getElementById(`${quizId}`).classList.add('active');
+    document.getElementById('quizName').innerHTML = document.getElementById(quizId).innerHTML;
+    document.getElementById('questionBody').style = 'display: none;';
+    document.getElementById('startScreen').style = '';
+    document.getElementById('endScreen').style = 'display: none;';
+    loadQuestions(quizId);   
+    showQuestion();
+}
+
+function loadQuestions(quizId) {
+    switch(quizId.slice(-1)) {
+        case '1':
+            questions = questionsQuiz1;
+            break;
+        case '2':
+            //questions = questionsQuiz2;
+            console.log('Quiz 2');
+            break;
+        case '4':
+            questions = questionsQuiz4;
+            break;
+    }
+}
+
+function startQuiz() {
+    document.getElementById('questionBody').style = '';
+    document.getElementById('startScreen').style = 'display: none;';
+    document.getElementById('endScreen').style = 'display: none;';
 }
